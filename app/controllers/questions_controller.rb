ddf7a82ff21b8f48c44b58b680c28b7669bddb67
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2021-08-09 16:05:33 +0200

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,16 @@ class QuestionsController < ApplicationController
   end
 
   def anwser
+     @userQuestion = params[:question]
+     @coach_anwser = ""
+
+
+     if @userQuestion.length - 1 == "?"
+      @coach_anwser = "Silly question, get dressed and go to work!"
+    elsif @userQuestion == "I am going to work"
+      @coach_anwser = "Great!"
+    else
+      @coach_anwser = "I don't care, get dressed and go to work!"
+     end
   end
 end
